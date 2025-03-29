10b090044646c67ca9d216e4a5152e3ea620916a
Luis Solorzano <luisnic20@gmail.com>
2025-03-29 11:18:21 -0600

    Updating templates

--- src/main.js
+++ src/main.js
@@ -11,17 +11,23 @@ module.exports = (vemto) => {
       let models = vemto.getProjectModels()
       let projectCruds = vemto.getProject().getMainCruds()
 
-      vemto.log.warning(`Generate TypeScript Entity`)
+      vemto.log.warning(`Generate React Starter Kit`)
+      let basePath = "resources/js/pages"
 
-      let basePath = 'app/TypeScript', options = { formatAs: 'ts', data: {} }
+      /**
+       * Generate entities.
+       */
       models.forEach(model => {
         options.data = {
           model
         }
 
-        vemto.renderTemplate('files/TypeScriptEntity.vemtl', `${basePath}/entities/${model.name.case('pascalCase')}.entity.ts}`, options)
+        vemto.renderTemplate('files/TypeScriptEntity.vemtl', `resources/js/types/${model.name.case('pascalCase')}.entity.ts}`, options)
       })
 
+      /**
+       * Generate crud
+       */
       projectCruds.forEach(crud => {
         const data = {
           model: {
@@ -34,17 +40,17 @@ module.exports = (vemto) => {
           }
         }
 
-        let reactPath = `resources/js/pages/${crud.url}`
+        let crudPath = `/${crud.url}`
         let controllerPath = `app/Http/Controllers/${crud.model.plural.case('pascalCase')}`
         let routePath = `routes`
         options.data = data
 
-        vemto.renderTemplate('files/pages/Form.vemtl', `${reactPath}/form/${crud.model.name.case('pascalCase')}Form.tsx`, options)
-        vemto.renderTemplate('files/pages/Table.vemtl', `${reactPath}/table/${crud.model.name.case('pascalCase')}Table.tsx`, options)
-        vemto.renderTemplate('files/pages/Show.vemtl', `${reactPath}/show.tsx`, options)
-        vemto.renderTemplate('files/pages/Index.vemtl', `${reactPath}/index.tsx`, options)
-        vemto.renderTemplate('files/pages/Edit.vemtl', `${reactPath}/edit.tsx`, options)
-        vemto.renderTemplate('files/pages/Create.vemtl', `${reactPath}/create.tsx`, options)
+        vemto.renderTemplate('files/pages/Form.vemtl', `${crudPath}/form/${crud.model.name.case('pascalCase')}Form.tsx`, options)
+        vemto.renderTemplate('files/pages/Table.vemtl', `${crudPath}/table/${crud.model.name.case('pascalCase')}Table.tsx`, options)
+        vemto.renderTemplate('files/pages/Show.vemtl', `${crudPath}/show.tsx`, options)
+        vemto.renderTemplate('files/pages/Index.vemtl', `${crudPath}/index.tsx`, options)
+        vemto.renderTemplate('files/pages/Edit.vemtl', `${crudPath}/edit.tsx`, options)
+        vemto.renderTemplate('files/pages/Create.vemtl', `${crudPath}/create.tsx`, options)
         vemto.renderTemplate('files/pages/Controller.vemtl', `${controllerPath}/${crud.model.getControllerName()}.php`, options)
         vemto.renderTemplate('files/pages/Route.vemtl', `${routePath}/${crud.url}.php`, options)
       })
